d0c226c44752a68ed8c63ebb8d16b218c5370aee
明日香-.- <arsnowyizumi@gmail.com>
2020-11-19 23:38:10 +0800

    first commir

---
+++ TubeLight/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NewMethod();
+    }
+
+    private void NewMethod()
+    {
+        gameObject.transform.Rotate(new Vector3(0, 1, 0));
+    }
+}
---
+++ TubeLight/move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    private Rigidbody mrigid;
+    int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mrigid = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+        this.mrigid.AddForce(new Vector3(0, 0.1f, 0.1f));
+    }
+
+}
